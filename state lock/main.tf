5e2225396338dc1592ad50b7ca31bcc7cd7494b2
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-02 11:33:57 +0530

    Update main.tf

--- state lock/main.tf
+++ state lock/main.tf
@@ -1,1 +1,24 @@
+vi main.tf
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+
+    bucket         = "terraform-statefile-examplee"
+    key            = "prod/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "dynamodb-terraform-state-locks"
+    region         = "ap-south-1"
+  }
+}
+resource "aws_instance" "myfirstinstance" {
+  ami           = "ami-0492447090ced6eb5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "backend-example1"
+  }
+}
 
