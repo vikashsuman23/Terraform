f7d792eda654eec27b686e05902b298fd83ef9a2
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-02 11:34:57 +0530

    Update main.tf

--- state lock/main.tf
+++ state lock/main.tf
@@ -15,8 +15,8 @@ terraform {
   }
 }
 resource "aws_instance" "myfirstinstance" {
-  ami           = "ami-0492447090ced6eb5"
-  instance_type = "t2.micro"
+  ami           = "ami-00ca570c1b6d79f36"
+  instance_type = "t3.micro"
   tags = {
     Name = "backend-example1"
   }
