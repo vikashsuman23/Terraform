59514342143c23bd70b3722a4011ee84df232a49
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-02 17:48:39 +0530

    Update variables.tf

--- Project1_VPC_EC2/variables.tf
+++ Project1_VPC_EC2/variables.tf
@@ -1,33 +1,44 @@
 variable "aws_region" { 
 type = string 
+default     = ""
 }
 variable "vpc_cidr" { 
 type = string 
+default     = ""
 }
 variable "subnet1_cidr" { 
-type = string
+type = string 
+default     = ""
 }
 variable "subnet2_cidr" { 
 type = string 
+default     = ""
 }
 variable "az1" { 
 type = string 
+default     = ""
 }
 variable "az2" { 
-type = string
+type = string 
+default     = ""
 }
 variable "ami_id" { 
 type = string 
+default     = ""
 }
 variable "instance_type" { 
 type = string 
+default     = ""
 }
 variable "bucket_name" { 
-type = string
+type = string 
+default     = ""
 }
 variable "alb_name" { 
 type = string 
+default     = ""
 }
 variable "target_group_name" { 
 type = string 
+default     = ""
 }
