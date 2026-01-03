4872cc198249823214763b30734f0695d64bb397
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-03 18:59:56 +0530

    Update main.tf

--- Project1_VPC_EC2/main.tf
+++ Project1_VPC_EC2/main.tf
@@ -28,7 +28,7 @@ resource "aws_route_table" "RT" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.route_cidr
     gateway_id = aws_internet_gateway.igw.id
   }
 }
