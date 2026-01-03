bf0e5ba9072dabecdda3c72c3b276eefedccb3dd
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-03 19:29:19 +0530

    Update backend.tf

--- Project2-RDS-TF/backend.tf
+++ Project2-RDS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-s3-backend-bucket"
-    key    = "prod/terraform.tfstate"
+    key    = "RDS_prod/terraform.tfstate"
     region = "ap-south-1"
   }
 }
