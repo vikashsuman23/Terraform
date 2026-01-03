a86697f210b85c6972b139c5df90803fcbc9070e
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-03 19:18:14 +0530

    Update variables.tf

--- Project2-RDS-TF/variables.tf
+++ Project2-RDS-TF/variables.tf
@@ -1,41 +1,41 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "ap-south-1"
+  
 }
 
 variable "db_instance_class" {
   description = "The instance type for the RDS instance"
   type        = string
-  default     = "db.t3.micro"
+
 }
 
 variable "db_name" {
   description = "The name of the database to be created"
   type        = string
-  default     = "mydatabase"
+  
 }
 
 variable "db_engine" {
   description = "The database engine to use"
   type        = string
-  default     = "postgres"
+
 }
 
 variable "db_engine_version" {
   description = "The version of the database engine"
   type        = string
-  default     = "15.7"
+  
 }
 variable "db_engine_pggroup" {
   description = "The version of the parameter group"
   type        = string
-  default     = "15"
+  
 }
 variable "db_username" {
   description = "The master username for the RDS instance"
   type        = string
-  default     = "reyaz"
+  
 }
 
 variable "db_password" {
@@ -46,7 +46,7 @@ variable "db_password" {
 variable "allocated_storage" {
   description = "The allocated storage in GB"
   type        = number
-  default     = 20
+ 
 }
 
 variable "vpc_id" {
@@ -62,17 +62,17 @@ variable "subnet_ids" {
 variable "db_backup_retention_period" {
   description = "The number of days to retain backups"
   type        = number
-  default     = 7
+ 
 }
 
 variable "db_backup_window" {
   description = "The daily time range during which automated backups are created"
   type        = string
-  default     = "03:00-04:00"
+ 
 }
 
 variable "db_maintenance_window" {
   description = "The weekly time range during which system maintenance can occur"
   type        = string
-  default     = "sun:05:00-sun:06:00"
+ 
 }
