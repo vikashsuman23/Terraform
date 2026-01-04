354f8ae712d54e232c016222c3a106f4a090a2b3
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-04 13:43:10 +0530

    Update main.tf

--- Project5_Mini_Module_project/main.tf
+++ Project5_Mini_Module_project/main.tf
@@ -1,23 +1,18 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 module "vpc" {
   source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cider
 }
 
 module "ec2" {
   source        = "./modules/ec2-instances"
-  ami           = "ami-02ddb77f8f93ca4ca"       # Amazon Linux 2023 in ap-south-1
-  instance_type = "t3.micro"
+  ami           = var.ami      # Amazon Linux 2023 in ap-south-1
+  instance_type = var.instance_type
   subnet_id     = module.vpc.public_subnet_id  # public_subnet_id is a output of VPC module output.tf. Output of VPC module is the input here
 }
 
 module "s3" {
   source      = "./modules/s3-bucket"
-  bucket_name = "my-demo-bucket-bkt-test-1"     # Change to a unique bucket name
+  bucket_name = var.bucket_name    # Change to a unique bucket name
 }
-
 
 
