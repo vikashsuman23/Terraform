016f9cc8129964988a63bdc49e07e323ee9c7f12
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-04 16:36:57 +0530

    Update output.tf

--- Project1_VPC_EC2/output.tf
+++ Project1_VPC_EC2/output.tf
@@ -1,4 +1,4 @@
 output "load_balancer_dns_name" {
   description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.application_lb.dns_name
+  value       = aws_lb.myalb.dns_name
 }
