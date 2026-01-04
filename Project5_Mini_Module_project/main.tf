a18a9ee39d1c1828d99fd338b9498a0f52f17a95
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-04 13:05:46 +0530

    Update main.tf

--- Project5_Mini_Module_project/main.tf
+++ Project5_Mini_Module_project/main.tf
@@ -19,12 +19,5 @@ module "s3" {
   bucket_name = "my-demo-bucket-bkt-test-1"     # Change to a unique bucket name
 }
 
-output "instance_id" {
-  value = module.ec2.instance_id  #Output.tf from EC2 module. In ec2-instance module, output.tf has instance_id. As we use modules
-                                  we should use output of ec2 module. if you use .id it will print id of module "ec2" as ec2.
-}
 
-output "bucket_arn" {
-  value = module.s3.bucket_arn
-}
 
