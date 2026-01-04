974b7876e5c767f5ccb194f13f9339bbbf4ce788
vikashsuman23 <82258076+vikashsuman23@users.noreply.github.com>
2026-01-04 16:31:23 +0530

    Update backend.tf

--- Project1_VPC_EC2/backend.tf
+++ Project1_VPC_EC2/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-s3-backend-bucket-20260104"
     key    = "prod/terraform.tfstate"
-    region = var.aws_region
+    region = "ap-south-1"
   }
 }
